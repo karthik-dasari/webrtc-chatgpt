ab8cd0439f459fa6964e444dacc4af09fd8345ad
karthik-skylark <karthik.dasari@skylarklabs.ai>
2023-05-18 12:17:46 +0530

    0.1

--- voice_call_project/settings.py
+++ voice_call_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-hh1wolz10)zx+h#f&^1q_0yn%p0@q&)+&8j*o^*ts@e-_on54v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
