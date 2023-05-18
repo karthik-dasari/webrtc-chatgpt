af908e2fb8232c07bf64248db91d3ffed6e0d687
karthik-skylark <karthik.dasari@skylarklabs.ai>
2023-05-18 13:17:38 +0530

    0.1

--- voice_call_project/settings.py
+++ voice_call_project/settings.py
@@ -64,7 +64,7 @@ ROOT_URLCONF = 'voice_call_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -125,7 +125,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
