c0ef3133227d27d09542ef27f2e281544cb26f13
karthik-skylark <karthik.dasari@skylarklabs.ai>
2023-05-18 12:57:34 +0530

    0.1

--- voice_call_project/settings.py
+++ voice_call_project/settings.py
@@ -87,9 +87,6 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-        'OPTIONS': {
-            'deterministic': False,
-        },
     }
 }
 
